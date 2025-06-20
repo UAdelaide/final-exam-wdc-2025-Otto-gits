37bc4a158ac3339b6d319a9cc747b25fecb796a7
Otto <161542360+Otto-gits@users.noreply.github.com>
2025-06-20 08:02:49 +0000

    [automatic commit] 2025-06-20 08:02:49 UTC 20.227.140.176

--- part2/models/db.js
+++ part2/models/db.js
@@ -1,13 +1,12 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
-  database: 'DogWalkService'
-  // ,
-  // waitForConnections: true,
-  // connectionLimit: 10,
-  // queueLimit: 0
+  database: 'DogWalkService',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = pool;
