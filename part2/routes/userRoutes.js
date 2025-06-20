e59989009291c62dfdc11c6d4e62321bd115c494
Otto <161542360+Otto-gits@users.noreply.github.com>
2025-06-20 08:59:40 +0000

    [automatic commit] 2025-06-20 08:59:40 UTC 20.227.140.176

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -63,6 +63,7 @@ router.post('/login', async (req, res) => {
     res.status(500).json({ error: 'Login failed' });
   }
 });
+
 router.post('/logout', (req, res) => {
   // destroy the session
   req.session.destroy();
