3941d13a5de4012edabc0152b57782d7a9af9856
Otto <161542360+Otto-gits@users.noreply.github.com>
2025-06-20 04:36:51 +0000

    [automatic commit] 2025-06-20 04:36:51 UTC 20.227.140.176

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -6,7 +6,7 @@ var db = require('../db');
 router.get('/dogs', async (req, res) => {
     const [rows] = await db.query(`
         SELECT
-        d.name  AS dog_name,
+        d.name AS dog_name,
         d.size,
         u.username AS owner_username
         FROM Dogs d
