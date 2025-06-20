df37e713ac59a5a97a2afa44bb505b8e87b56944
Otto <161542360+Otto-gits@users.noreply.github.com>
2025-06-20 10:05:32 +0000

    [automatic commit] 2025-06-20 10:05:32 UTC 20.227.140.176

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -32,7 +32,7 @@ router.get('/me', (req, res) => {
   if (!req.session.user) {
     return res.status(401).json({ error: 'Not logged in' });
   }
-  res.json(req.session.user);
+  return res.json(req.session.user);
 });
 
 router.post('/login', async (req, res) => {
