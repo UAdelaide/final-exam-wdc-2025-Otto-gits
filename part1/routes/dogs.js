7460356e872c75239dd4d7b0fd3457903df73df0
Otto <161542360+Otto-gits@users.noreply.github.com>
2025-06-20 04:34:31 +0000

    [automatic commit] 2025-06-20 04:34:31 UTC 20.227.140.176

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -32,5 +32,21 @@ router.get('/walkrequests/open', async (req, res) => {
         `);
     res.json(rows);
 });
+router.get('/walkrequests/open', async (req, res) => {
+    const [rows] = await db.query(`
+        SELECT
+        rq.request_id,
+        d.name,
+        rq.requested_time,
+        rq.duration_minutes,
+        rq.location,
+        u.username
+        FROM WalkRequests rq
+        JOIN Dogs d ON rq.dog_id = d.dog_id
+        JOIN Users u ON d.owner_id = u.user_id
+        WHERE rq.status = 'open'
+        `);
+    res.json(rows);
+});
 
 module.exports = router;
