a5258148e2cc2a29ec6af4cf2ef5ad2d1f0f9488
Otto <161542360+Otto-gits@users.noreply.github.com>
2025-06-20 07:18:41 +0000

    [automatic commit] 2025-06-20 07:18:41 UTC 20.227.140.176

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -54,12 +54,7 @@ router.post('/login', async (req, res) => {
     if (user.role === 'walker') {
       return res.redirect('/walker-dashboard.html');
     }
-
-    if (user.role === 'owner'){
-      return res.redirect('/owner-dashboard.html');
-    }
-
-    return res.json({ message: 'Login successful', user: rows[0] });
+    return res.redirect('/owner-dashboard.html');
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
   }
